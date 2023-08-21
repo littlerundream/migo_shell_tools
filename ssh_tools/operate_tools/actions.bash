32e96f04bca55e3839f20fc9c6b5a170e9aa985e
littlerundream <128053055+littlerundream@users.noreply.github.com>
2023-08-21 06:51:44 +0000

    [feat]添加配置锁保护

--- ssh_tools/operate_tools/actions.bash
+++ ssh_tools/operate_tools/actions.bash
@@ -2,3 +2,16 @@ function quitScript()
 {
   clear
 }
+
+function runArthasLocal()
+{
+   java -jar ~/arthas-boot.jar `ps axu | grep jar | grep Bootstrap | awk '{print $2}'`
+}
+
+function checkTheProtectionLock()
+{
+  if [ -f ./.lock ]; then
+    echo '配置处在锁定保护状态，无法切换'
+    exit 1
+  fi
+}
